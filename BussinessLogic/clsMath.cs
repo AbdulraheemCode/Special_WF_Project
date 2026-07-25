6875eb6e0ecc94ee7ef2717cfe1b5f8180ef1f74
Abdulraheem AlHashimi <akhpr92@gmail.com>
2026-07-25 05:29:00 +0300

    Add Bussiness Logic

---
+++ BussinessLogic/clsMath.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic
+{
+    public class clsMath
+    {
+
+        public static double Add(double x , double y)
+        {
+            return x + y;
+        }
+
+        public static void PrintString(string str)
+        {
+            Console.WriteLine($"Your String : {str}"); 
+        }
+
+
+
+
+    }
+}
