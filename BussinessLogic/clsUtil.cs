aba0d6c28d1c2cdb82d10222a8aad9e9b4a1efe3
Abdulraheem AlHashimi <akhpr92@gmail.com>
2026-07-29 17:31:37 +0300

    Add Util Class

---
+++ BussinessLogic/clsUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLogic
+{
+    public class clsUtil
+    {
+
+        public static void Fun1()
+        {
+            Console.WriteLine("This is Func (1)");
+        }
+
+
+
+
+    }
+}
